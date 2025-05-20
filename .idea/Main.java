4d466d72af55dc2c951bac356481bfbca453d1da
khosatho <monokhosatho@gmail.com>
2025-05-20 13:05:03 +0200

    added DRIVER

--- .idea/Main.java
+++ .idea/Main.java
@@ -1,3 +1,11 @@
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        String url = "https://www.google.com";
+        String username = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+        String password = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+
+        try {
+
+        }
+    }
 }
